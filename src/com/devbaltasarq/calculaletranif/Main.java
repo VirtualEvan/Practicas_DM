de7574c26cc0381e74f0baca92472dd0dadfbfd9
baltasarq <baltasarq@gmail.com>
2015-10-04 11:27:29 +0200

    Better listener and a toast

--- src/com/devbaltasarq/calculaletranif/Main.java
+++ src/com/devbaltasarq/calculaletranif/Main.java
@@ -1,27 +1,43 @@
 package com.devbaltasarq.calculaletranif;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 
 /** Actividad de arranque de la app */
 public class Main extends Activity {
     @Override
     public void onCreate(Bundle appState) {
-        super.onCreate( appState );
-        this.setContentView( R.layout.main );
+        super.onCreate(appState);
+        this.setContentView(R.layout.main);
 
+        // Link to events
         EditText edDni = (EditText) this.findViewById( R.id.edDni );
-        edDni.setOnKeyListener( new View.OnKeyListener() {
+        edDni.addTextChangedListener( new TextWatcher() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                Main.this.calcula();
-                return false;
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
-        } );
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Main.this.calcula();
+            }
+        });
     }
 
     private void calcula() {
@@ -29,11 +45,17 @@ public class Main extends Activity {
         TextView lblResult = (TextView) this.findViewById( R.id.lblResult );
 
         try {
-            int dni = Integer.parseInt( edDni.getText().toString() );
-            lblResult.setText( Character.toString( CalculoLetraNif.calculaLetraNif( dni ) ) );
+            String value = edDni.getText().toString();
+            int dni = Integer.parseInt( value );
+            String result = value;
+            result += Character.toString(com.devbaltasarq.calculaletranif.CalculoLetraNif.calculaLetraNif(dni));
+            lblResult.setText(result);
         }
         catch(NumberFormatException exc) {
             lblResult.setText( R.string.label_default_result );
+            Toast.makeText( this.getApplicationContext(), R.string.label_default_result, Toast.LENGTH_SHORT ).show();
         }
     }
+
+    private int opc;
 }
